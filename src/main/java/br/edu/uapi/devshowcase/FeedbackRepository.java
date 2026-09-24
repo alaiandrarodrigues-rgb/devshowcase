1c720a9e45b72537ec56d4c8877d4a9f860de271
alaiandrarodrigues-rgb <alaiandrarodrigues@gmail.com>
2026-09-23 22:50:52 -0300

    Update FeedbackRepository.java

--- src/main/java/br/edu/uapi/devshowcase/FeedbackRepository.java
+++ src/main/java/br/edu/uapi/devshowcase/FeedbackRepository.java
@@ -1,8 +1,13 @@
 package br.edu.uapi.devshowcase;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+
     long countByProject(Project project);
-    Double findAverageRatingByProject(Project project);
+
+    @Query("SELECT AVG(f.rating) FROM Feedback f WHERE f.project = :project")
+    Double findAverageRatingByProject(@Param("project") Project project);
 }
